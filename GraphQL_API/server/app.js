dacbce0426141acf79ca9dfefbbd6f5ff3e8ba44
BoostedT <tyler.whitchurch@atlasschool.com>
2025-07-14 16:27:40 -0500

    finished up some task will need to recheck them

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  'mongodb+srv://admin:MW6UpYSiLWPhTFgO@graphql.8mmtnvr.mongodb.net/?retryWrites=true&w=majority&appName=Graphql',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 const app = express();
 
